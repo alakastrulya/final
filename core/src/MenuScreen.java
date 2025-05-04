9ac53ee18269611beeead18b8317c4e8ff2be004
Aisha Akhmetova <noviikoteeenn@gmail.com>
2025-05-04 23:14:04 +0600

    Adding MenuScreen with basic methods

---
+++ core/src/MenuScreen.java
@@ -0,0 +1,27 @@
+import com.badlogic.gdx.Screen;
+
+public class MenuScreen implements Screen {
+    @Override
+    public void resize(int width, int height){
+    }
+
+    @Override
+    public void pause(){
+    }
+
+    @Override
+    public void resume(){
+    }
+
+    @Override
+    public void show(){
+    }
+
+    @Override
+    public void hide(){
+    }
+
+    @Override
+    public void dispose(){
+    }
+}
